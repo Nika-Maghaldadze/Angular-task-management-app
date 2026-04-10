6fd098fb805b7fd1ce321f6aae81512423dbb3d8
NikaMaghaldadze <nikamaghaldadze13@gmail.com>
2026-04-10 13:54:36 +0400

    adding tasks functionality

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { newTaskComponent } from './new-task/new-task.component';
+import { type newTaskData } from './task/task.model';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, newTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask: boolean = false;
   tasks = [
     {
       id: 't1',
@@ -104,7 +107,24 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 
-  removeCompleted(id:string){
-    this.tasks = this.tasks.filter(task => task.id !== id);
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+  onAddTask(taskData: newTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId:this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+    this.isAddingTask = false
+  }
+
+  removeCompleted(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
